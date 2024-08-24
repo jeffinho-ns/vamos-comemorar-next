320cdebd4f994a20ec367821fcc2de08125a43d2
Jefferson Lima <82480661+jeffinho-ns@users.noreply.github.com>
2024-08-23 22:17:11 -0700

    Ajustes nos arquivos para testar o deploy

---
+++ app/models/IBanner.ts
@@ -0,0 +1,7 @@
+// app/models/IBanner.ts
+export interface IBannerProps {
+    // Defina as propriedades aqui
+    title: string;
+    imageUrl: string;
+    // Outras propriedades que você precisa
+  }
