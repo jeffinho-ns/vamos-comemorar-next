54127dbff302588bd867fcfdb13d4fee6a82c3bc
Jefferson Lima <82480661+jeffinho-ns@users.noreply.github.com>
2025-01-16 02:16:56 -0800

    Ajuste na home

--- app/webapp/page.tsx
+++ app/webapp/page.tsx
@@ -1,24 +1,23 @@
 "use client";
 
-import imgBanner from "@/app/assets/retangulo.png";
 import React, { useEffect, useState, useCallback } from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
-import { MdSearch, MdFilterList } from 'react-icons/md'
+import { MdSearch, MdFilterList } from "react-icons/md";
 import { FaHeart } from "react-icons/fa";
 import { useRouter } from "next/navigation";
-import logoWhite from "@/app/assets/logo_blue.png";
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
-import "../webapp/global.scss";
-import Intro from "./components/intro/intro"; 
-
-// Importação das imagens das logos
+import Intro from "./components/intro/intro";
+import imgBanner from "@/app/assets/retangulo.png";
+import logoWhite from "@/app/assets/logo_blue.png";
 import Logo1 from "../webapp/assetsMobile/logos/justinologo.png";
 import Logo2 from "../webapp/assetsMobile/logos/logo-pracinha.png";
 import Logo3 from "../webapp/assetsMobile/logos/logoOhfregues.png";
 import Logo4 from "../webapp/assetsMobile/logos/highlinelogo.png";
+
+import "../webapp/global.scss";
 
 interface Event {
   id: string;
@@ -28,21 +27,19 @@ interface Event {
   nome_do_evento: string;
   casa_do_evento: string;
   local_do_evento: string;
+  data_do_evento: string;
   place: {
     name: string;
     logo: string;
   };
 }
 
-
 export default function Home() {
   const [events, setEvents] = useState<Event[]>([]);
   const [loading, setLoading] = useState(true);
-  const [isClient, setIsClient] = useState(false);
-  const [showIntro, setShowIntro] = useState(true); // Estado para gerenciar o Intro
-  const [currentIntroPage, setCurrentIntroPage] = useState(0); // Página atual do Intro
+  const [showIntro, setShowIntro] = useState(true);
+  const [currentIntroPage, setCurrentIntroPage] = useState(0);
   const API_URL = process.env.NEXT_PUBLIC_API_URL || process.env.NEXT_PUBLIC_API_URL_LOCAL;
-
   const router = useRouter();
 
   const fetchEvents = useCallback(() => {
@@ -60,9 +57,8 @@ export default function Home() {
   }, [API_URL]);
 
   useEffect(() => {
-    // Se showIntro é falso, certifique-se de que o estado não mude
     if (showIntro) {
-      localStorage.setItem("introShown", "true"); // Marcar como exibido
+      localStorage.setItem("introShown", "true");
     }
   }, [showIntro]);
 
@@ -74,9 +70,16 @@ export default function Home() {
     }
   };
 
+
   useEffect(() => {
     fetchEvents();
   }, [fetchEvents]);
+
+  const formatDate = (dateString: string) => {
+    const date = new Date(dateString);
+    const options: Intl.DateTimeFormatOptions = { month: 'short', day: '2-digit' };
+    return date.toLocaleDateString('pt-BR', options);
+  };
 
   const Card: React.FC<{ event: Event }> = ({ event }) => {
     const getEventPagePath = (place: string) => {
@@ -102,8 +105,7 @@ export default function Home() {
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5, ease: "easeOut" }}
         >
-          <div className="relative w-full h-[550px]">
-            <div className="absolute inset-0 bg-black opacity-50 z-10" />
+          <div className="relative w-full h-[250px]">
             <Image
               src={`${API_URL}/uploads/events/${event.imagem_do_evento}`}
               alt={event.nome_do_evento}
@@ -112,38 +114,54 @@ export default function Home() {
               className="absolute inset-0 w-full h-full object-cover"
               unoptimized
             />
-            <div className="absolute bottom-4 left-4 text-shadow z-20">
-              <h2 className="text-white text-xl font-bold mb-1">{event.casa_do_evento}</h2>
-              <p className="text-white">{event.local_do_evento}</p>
-            </div>
-            <div className="absolute bottom-4 right-4 bg-white rounded-full p-2 z-20">
-              <FaHeart className="text-red-500 text-2xl" />
-            </div>
           </div>
-          <div className="card-button">Reservar</div>
+          <div className="p-4">
+            <h2 className="text-xl font-bold">{event.casa_do_evento}</h2>
+            
+            <div className="flex items-center mt-2">
+              <div className="flex -space-x-2 overflow-hidden">
+                <img
+                  className="inline-block h-8 w-8 rounded-full ring-2 ring-white"
+                  src="/path-to-avatar1.jpg"
+                  alt="User 1"
+                />
+                <img
+                  className="inline-block h-8 w-8 rounded-full ring-2 ring-white"
+                  src="/path-to-avatar2.jpg"
+                  alt="User 2"
+                />
+              </div>
+              <p className="text-sm text-gray-500 ml-2">+20 Going</p>
+            </div>
+            <p className="text-sm text-gray-400 mt-2">{event.local_do_evento}</p>
+          </div>
+          <div className="absolute top-2 left-2 bg-white rounded-full p-2">
+          <span className="text-xs font-bold text-red-500">{formatDate(event.data_do_evento)}</span>
+          </div>
+          <div className="absolute top-2 right-2 bg-white rounded-full p-2">
+            <FaHeart className="text-red-500 text-2xl" />
+          </div>
         </motion.div>
       </Link>
     );
   };
 
   return (
-    
     <>
-{loading && (
-  <div className="absolute inset-0 bg-black bg-opacity-60 flex items-center justify-center z-50">
-    <Intro />
-  </div>
-)}
+      {loading && (
+        <div className="absolute inset-0 bg-black bg-opacity-60 flex items-center justify-center z-50">
+          <Intro />
+        </div>
+      )}
       <Header className="z-20" />
-
       <div className="relative">
         <div id="home-container" className="container-mobile relative z-1">
-        <div className="flex flex-col items-center mt-8 z-10">
+          <div className="flex flex-col items-center mt-8 z-10">
             <Link href="/">
               <Image src={logoWhite} alt="Logo" className="w-[120px] h-auto" />
             </Link>
-          <p className="title text-white text-center mt-2 z-10">Qual evento você procura?</p>
-        </div>
+            <p className="title text-white text-center z-10">Qual evento você procura?</p>
+          </div>
           <div className="absolute inset-0 w-full h-[450px] z-0 mt-[-100px] rounded-[30px] overflow-hidden">
             <Image
               src={imgBanner}
@@ -154,60 +172,63 @@ export default function Home() {
               unoptimized
             />
           </div>
-          <div className="flex justify-center mt-8 z-10">
-  <form className="w-11/12 max-w-md flex items-center">
-    {/* Campo de busca */}
-    <div className="flex items-center flex-grow bg-transparent border-b border-gray-300">
-      <MdSearch className="text-gray-500 text-2xl mr-2" />
-      <input
-        placeholder="Search..."
-        type="text"
-        id="search"
-        className="w-full bg-transparent p-2 focus:outline-none text-gray-700 placeholder-gray-500"
-      />
-    </div>
-
-    {/* Botão de filtro */}
-    <button
-      type="button"
-      className="flex items-center bg-blue-600 text-white px-4 py-2 ml-4 rounded-full"
-    >
-      <MdFilterList className="text-xl mr-2" />
-      Filters
-    </button>
-  </form>
-</div>
-
-
+          <div className="flex justify-center z-10">
+            <form className="w-11/12 max-w-md flex items-center">
+              <div className="flex items-center flex-grow bg-transparent">
+                <MdSearch className="text-white text-4xl mr-2" />
+                <input
+                  placeholder="Buscar..."
+                  type="text"
+                  id="search"
+                  className="w-full bg-transparent p-2 focus:outline-none text-white placeholder-white"
+                />
+              </div>
+              <button
+                type="button"
+                className="flex items-center bg-blue-600/75 text-white px-4 py-2 ml-4 rounded-full backdrop-blur-sm hover:bg-blue-600/90 transition duration-300"
+              >
+                <MdFilterList className="text-xl mr-2" />
+                Filters
+              </button>
+            </form>
+          </div>
           <div className="flex justify-center gap-6 my-8">
             <Link href="/webapp/justino">
-              <div className="flex items-center justify-center w-20 h-20 bg-white rounded-full shadow-lg hover:shadow-xl transition-all duration-300 transform hover:scale-110">
+              <div className="flex items-center justify-center shadow-lg hover:shadow-xl transition-all duration-300 transform hover:scale-110">
                 <Image src={Logo1} alt="Justino" width={50} height={50} className="object-contain rounded-full" unoptimized />
               </div>
             </Link>
             <Link href="/webapp/pracinha">
-              <div className="flex items-center justify-center w-20 h-20 bg-white rounded-full shadow-lg hover:shadow-xl transition-all duration-300 transform hover:scale-110">
+              <div className="flex items-center justify-center shadow-lg hover:shadow-xl transition-all duration-300 transform hover:scale-110">
                 <Image src={Logo2} alt="Pracinha" width={50} height={50} className="object-contain rounded-full" unoptimized />
               </div>
             </Link>
             <Link href="/webapp/ohfregues">
-              <div className="flex items-center justify-center w-20 h-20 bg-white rounded-full shadow-lg hover:shadow-xl transition-all duration-300 transform hover:scale-110">
+              <div className="flex items-center justify-center shadow-lg hover:shadow-xl transition-all duration-300 transform hover:scale-110">
                 <Image src={Logo3} alt="Oh Freguês" width={50} height={50} className="object-contain rounded-full" unoptimized />
               </div>
             </Link>
             <Link href="/webapp/highline">
-              <div className="flex items-center justify-center w-20 h-20 bg-white rounded-full shadow-lg hover:shadow-xl transition-all duration-300 transform hover:scale-110">
+              <div className="flex items-center justify-center shadow-lg hover:shadow-xl transition-all duration-300 transform hover:scale-110">
                 <Image src={Logo4} alt="Highline" width={50} height={50} className="object-contain rounded-full" unoptimized />
               </div>
             </Link>
           </div>
-
+          <div className="flex justify-between items-center my-8 px-6">
+            <p className="text-lg font-semibold text-black">Próximos eventos</p>
+            <Link
+              href="/webapp/eventos"
+              className="text-[#747688] hover:text-[#747688] text-[10px] font-medium"
+            >
+              Ver todos
+            </Link>
+          </div>
           <div className="cards-container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 my-8">
             {loading
               ? [...Array(6)].map((_, index) => (
                   <div
                     key={index}
-                    className="border border-blue-300 shadow rounded-lg p-4 w-full h-[550px] mx-auto animate-pulse"
+                    className="border border-blue-300 shadow rounded-lg p-4 w-full h-[250px] mx-auto animate-pulse"
                   >
                     <div className="animate-pulse flex flex-col h-full">
                       <div className="bg-slate-700 h-3/4 w-full rounded-t-lg"></div>
@@ -223,8 +244,6 @@ export default function Home() {
           <Footer />
         </div>
       </div>
-
-      
     </>
   );
 }
