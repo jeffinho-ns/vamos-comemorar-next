99739b81d5c950ff517726befd1e5afb91d218b9
Jefferson Lima <82480661+jeffinho-ns@users.noreply.github.com>
2025-08-06 02:04:44 -0700

    fix: reverte configuração para Vercel deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    output: 'export',
-    trailingSlash: true,
     images: { 
-        unoptimized: true,
         domains: [
             "vamos-comemorar-api.onrender.com"  // Apenas o domínio da API no Render
         ],
